f7362acc9ac910766c7ed75ac28379d681982b19
wick <rakhmanov0410@gmail.com>
2024-10-20 00:40:25 +0500

    add project files

--- root/settings.py
+++ root/settings.py
@@ -216,5 +216,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'asadbekmehmonjonov5@gmail.com'
-EMAIL_HOST_PASSWORD = 'urfq uvhp vqpd vkek'
+EMAIL_HOST_USER = os.getenv('EMAIL')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_PASS')
