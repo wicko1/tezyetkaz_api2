b9e3f6c3b28f3b10efb1f3b8f79bd0cb642237df
wick <rakhmanov0410@gmail.com>
2024-10-30 13:51:10 +0500

    add project files

--- root/urls.py
+++ root/urls.py
@@ -10,5 +10,4 @@ urlpatterns = [
                   path('', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
                   path("admin/", admin.site.urls),
                   path("ckeditor5/", include('django_ckeditor_5.urls')),
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL,
-                                                                                         document_root=settings.STATIC_ROOT)
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
