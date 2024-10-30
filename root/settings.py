d5caec7355aefbe9c54e18100bc16c0eba2da1f0
wick <rakhmanov0410@gmail.com>
2024-10-30 09:17:13 +0500

    add project files

--- root/settings.py
+++ root/settings.py
@@ -205,7 +205,7 @@ CKEDITOR_5_CONFIGS = {
 }
 
 SPECTACULAR_SETTINGS = {
-    'TITLE': 'Tez - Yetkaz api real project',
+    'TITLE': 'Tez - Yetkaz api real project 1',
     'DESCRIPTION': 'Your project description',
     'VERSION': '1.0.0',
     'SERVE_INCLUDE_SCHEMA': False,
