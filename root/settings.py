4a89a45e41802e73cb38d3c36da0dbda09c124a4
wick <rakhmanov0410@gmail.com>
2024-10-30 13:53:17 +0500

    add project files

--- root/settings.py
+++ root/settings.py
@@ -107,12 +107,12 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-#
-# STATIC_URL = 'static/'
-# STATIC_ROOT = os.path.join(BASE_DIR / 'static')
-#
-# MEDIA_URL = "media/"
-# MEDIA_ROOT = os.path.join(BASE_DIR / 'media')
+
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR / 'static')
+
+MEDIA_URL = "media/"
+MEDIA_ROOT = os.path.join(BASE_DIR / 'media')
 
 REST_FRAMEWORK = {
     # YOUR SETTINGS
